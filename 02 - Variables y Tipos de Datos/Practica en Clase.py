b2e2b7c705b3b5d0057cc0b485009eae6a18e8d5
eristofferius <cristopher.moralesceron@outlook.com>
2023-02-25 00:47:16 -0300

    first commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -4,9 +4,9 @@ print(mi_variable)
 mi_variable2 = 'dario'
 print(mi_variable2)
 
-mi_complejo = 5 + 7j
-print(mi_complejo)
+mi_complejo1 = 5 + 7j
+print(mi_complejo1)
 
 
-mi_numero = input('Ingrese un valor')
+mi_numero = input('Ingrese un valop')
 print('el valor ingresado fue', mi_numero)
